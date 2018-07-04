1542bca2f41842d485cfd8b44c0c8bfe516ccd69
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2018-07-04 18:47:16 +0600

    upd

---
+++ Week4/54.cpp
@@ -0,0 +1,40 @@
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	string operation;
+	stack<int> st;
+
+	while(true){
+		cin >> operation;
+		if(operation == "exit") break;
+		if(operation == "push"){
+			int n;
+			cin >> n;
+			st.push(n);
+			cout << "ok" << endl;
+		}else if(operation == "pop"){
+			cout << st.top() << endl;
+			st.pop();
+		}else if(operation == "back"){
+			cout << st.top() << endl;
+		}else if(operation == "size"){
+			cout << st.size() << endl;
+		}else if(operation == "clear"){
+			
+			while(st.size() != 0){
+				st.pop();
+			}
+
+
+			cout << "ok" << endl;
+		}
+	}
+
+	cout << "bye" << endl;
+
+	return 0;
+}
